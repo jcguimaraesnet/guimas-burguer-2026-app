a5494850d272a0ff391e7adfecee1ad957b62d6b
Júlio César Guimarães <julio.guimaraes@prof.infnet.edu.br>
2026-05-06 07:26:06 -0300

    feat: listagem de hambuguer

---
+++ GuimasBurguer2026App/Models/Hamburguer.cs
@@ -0,0 +1,13 @@
+﻿namespace GuimasBurguer2026App.Models
+{
+    public class Hamburguer
+    {
+        public int HamburguerId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string ImagemUri { get; set; }
+        public double Preco { get; set; }
+        public bool EntregaExpressa { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
--- GuimasBurguer2026App/Pages/Index.cshtml.cs
+++ GuimasBurguer2026App/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using GuimasBurguer2026App.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,6 +7,7 @@ namespace GuimasBurguer2026App.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public IList<Hamburguer> Hamburguers { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,7 +16,39 @@ namespace GuimasBurguer2026App.Pages
 
         public void OnGet()
         {
-
+            Hamburguers = new List<Hamburguer>()
+            {
+                new Hamburguer
+                {
+                    HamburguerId = 1,
+                    Nome = "Beef Burger",
+                    Descricao = "Hambúrguer simples: suculento, saboroso e delicioso.",
+                    ImagemUri = "/imagens/beef-burger.jpg",
+                    DataCadastro = DateTime.Now,
+                    EntregaExpressa = true,
+                    Preco = 19.00,
+                },
+                new Hamburguer
+                {
+                    HamburguerId = 2,
+                    Nome = "Canoe Burger",
+                    Descricao = "Delicioso hambúrguer com batata canoa crocante. Uma explosão de sabores em cada mordida!",
+                    ImagemUri = "/imagens/beef-burger-canoe-potatoes.jpg",
+                    DataCadastro = DateTime.Now,
+                    EntregaExpressa = false,
+                    Preco = 29.00,
+                },
+                new Hamburguer
+                {
+                    HamburguerId = 3,
+                    Nome = "Pepper Burger",
+                    Descricao = "Hambúrguer irresistível com pimentão: sabor picante e suculento em cada pedaço!",
+                    ImagemUri = "/imagens/beef-burger-peppers.jpg",
+                    DataCadastro = DateTime.Now,
+                    EntregaExpressa = true,
+                    Preco = 39.00,
+                },
+            };
         }
     }
 }
